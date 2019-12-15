7115bd03b34d8089877a233218196e31ece4d83a
dabbasjo <abdallah.dabbas.93@hotmail.com>
2019-12-15 19:54:00 +0200

    update

---
+++ src/main/java/com/decoders/leaves/MyahonaLeavsTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.decoders.leaves;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyahonaLeavsTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyahonaLeavsTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/decoders/leaves/MyahonaLeavsTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.decoders.leaves;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyahonaLeavsTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
